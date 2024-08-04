03f002a2b3b375c07eea57b8176ad1ac9853fe0f
inoue.h <oasis3855@users.noreply.github.com>
2024-08-04 12:14:50 +0900

    Webpage Init

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # GitHub Pages の共通ディレクトリ
 
-``` gh-pages ``` ブランチ内に、GitHub Pages([https://oasis3855.github.io/webpage2/](https://oasis3855.github.io/webpage2/))で表示するファイルを格納している。
-
+``` gh-pages ``` ブランチ内に、GitHub Pagesで表示するファイルを格納している。
 
---
+++ docs/cgi-bin/fancybox-2.1.5/fancyboxOnload.js
@@ -0,0 +1,16 @@
+/* this JavaScript is created by MovableType template */
+jQuery(document).ready(function($){
+    $('a[href$=".jpg"], a.fancybox').fancybox({
+        arrows : true,
+        closeBtn : true,
+        loop : false,
+        openEffect : 'fade',
+        closeEffect : 'fade',
+        nextEffect : 'fade',
+        prevEffect : 'fade',
+        helpers : {
+            title : { type : 'outside' },
+            buttons : {}
+        }
+    });
+})
